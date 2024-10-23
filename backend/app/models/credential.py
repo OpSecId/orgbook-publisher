4d7d44c263bdd6166f395c5c60f9b570e15fc434
PatStLouis <patrick.st-louis@opsecid.ca>
2024-10-23 20:23:17 +0000

    fix credential data model
    Signed-off-by: PatStLouis <patrick.st-louis@opsecid.ca>

--- backend/app/models/credential.py
+++ backend/app/models/credential.py
@@ -87,7 +87,7 @@ class RenderMethod(BaseModel):
 
 class Credential(BaseModel):
     context: List[str] = Field(["https://www.w3.org/ns/credentials/v2"], alias="@context")
-    type: Union[str, List[str]] = Field("VerifiableCredential")
+    type: Union[str, List[str]] = Field(["VerifiableCredential"])
     validFrom: SkipJsonSchema[str] = Field(None)
     validUntil: SkipJsonSchema[str] = Field(None)
     credentialSubject: Union[List[CredentialSubject], CredentialSubject] = Field()
