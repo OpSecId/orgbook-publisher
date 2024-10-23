d4404343affb4db509d7473900eca82db696e744
PatStLouis <patrick.st-louis@opsecid.ca>
2024-10-23 19:35:21 +0000

    skip tdw provision
    Signed-off-by: PatStLouis <patrick.st-louis@opsecid.ca>

--- backend/main.py
+++ backend/main.py
@@ -4,7 +4,7 @@ from app.plugins import AskarStorage, TractionController
 
 if __name__ == "__main__":
     asyncio.run(AskarStorage().provision())
-    asyncio.run(TractionController().provision_tdw())
+    # asyncio.run(TractionController().provision_tdw())
     uvicorn.run(
         "app:app",
         host="0.0.0.0",
